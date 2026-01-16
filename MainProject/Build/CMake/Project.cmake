559e9b0f6cafa9e1aa32a823a631e9ece531e50d
NtWanderer <228698745+NtWanderer@users.noreply.github.com>
2026-01-16 13:43:06 -0300

    Added basic CMakeLists.txt functionality alongside Registry and Project script files.

---
+++ MainProject/Build/CMake/Project.cmake
@@ -0,0 +1,91 @@
+# Copyright (c) Mirage Engine. All Rights Reserved.
+
+include_guard()
+
+include("Registry")
+
+#
+# Project Directories
+#
+
+set(PROJECT_BINARIES_DIRECTORY		"Binaries")
+set(PROJECT_BUILD_DIRECTORY			"Build")
+set(PROJECT_CONTENT_DIRECTORY		"Content")
+set(PROJECT_DOCUMENTATION_DIRECTORY	"Documentation")
+set(PROJECT_PLUGINS_DIRECTORY		"Plugins")
+set(PROJECT_PROGRAMS_DIRECTORY		"Programs")
+set(PROJECT_SHADERS_DIRECTORY		"Shaders")
+set(PROJECT_SOURCE_DIRECTORY		"Source")
+
+#
+# Project Build Subdirectories
+#
+
+set(PROJECT_BUILD_LIBRARIES_DIRECTORY		"${PROJECT_BUILD_DIRECTORY}/Libraries")
+set(PROJECT_BUILD_INTERMEDIATES_DIRECTORY	"${PROJECT_BUILD_DIRECTORY}/Intermediates")
+
+#
+# Project Build/Intermediates Subdirectories
+#
+
+set(PROJECT_BUILD_INTERMEDIATES_BUILDFILES_DIRECTORY    "${PROJECT_BUILD_INTERMEDIATES_DIRECTORY}/BuildFiles")
+set(PROJECT_BUILD_INTERMEDIATES_PROJECTFILES_DIRECTORY  "${PROJECT_BUILD_INTERMEDIATES_DIRECTORY}/ProjectFiles")
+
+#
+# Project Helper Functions
+#
+
+# Add a Project to the Project list Registry.
+function(AddProjectList InProjectName)
+	AddRegistryList(PROJECT ${InProjectName})
+endfunction()
+
+# Get the Project list Registry.
+function(GetProjectList OutValue)
+	GetRegistryList(Value PROJECT)
+	set(${OutValue} "${Value}" PARENT_SCOPE)
+endfunction()
+
+# Set a Property value on the Project Registry.
+function(SetProjectEntry InProjectName InPropertyName InValue)
+	SetRegistryEntry(PROJECT ${InProjectName} ${InPropertyName} ${InValue})
+endfunction()
+
+# Get a Property value from the Project Registry.
+function(GetProjectEntry OutValue InProjectName InPropertyName)
+	GetRegistryEntry(Value PROJECT ${InProjectName} ${InPropertyName})
+	set(${OutValue} "${Value}" PARENT_SCOPE)
+endfunction()
+
+#
+# Project Properties
+#
+
+# Add a Project to the Project list Registry and set the Project directory.
+function(AddProject InProjectName InProjectDirectory)
+	#
+    # Check if already registered using the Registry helper
+	#
+
+    GetProjectList(ProjectList)
+
+    if("${InProjectName}" IN_LIST ProjectList)
+        message(Warning "Project [${InProjectName}] already registered.")
+        return()
+    endif()
+
+	#
+    # Use the helper to add to the global Registry list
+	#
+
+    AddProjectList("${InProjectName}")
+
+	#
+    # Store the absolute directory context
+	#
+
+    get_filename_component(AbsoluteDir "${InProjectDirectory}" ABSOLUTE)
+    SetProjectEntry("${InProjectName}" "Directory" "${AbsoluteDir}")
+
+    message(Info "Registered Project: ${InProjectName} -> ${AbsoluteDir}")
+endfunction()
---
+++ MainProject/Build/CMake/Registry.cmake
@@ -0,0 +1,55 @@
+# Copyright (c) Workspace. All Rights Reserved.
+
+include_guard()
+
+#
+# Registry list access
+#
+
+# Add a specified Registry to the RegistryType list.
+macro(AddRegistryList InRegistryType InRegistryName)
+    get_property(GlobalList GLOBAL PROPERTY REGISTRY_${InRegistryType}_LIST)
+
+    if(NOT "${InRegistryName}" IN_LIST GlobalList)
+        set_property(GLOBAL APPEND PROPERTY REGISTRY_${InRegistryType}_LIST "${InRegistryName}")
+    endif()
+endmacro()
+
+# Get the actual RegistryType list.
+macro(GetRegistryList OutVar InRegistryType)
+    get_property(${OutVar} GLOBAL PROPERTY REGISTRY_${InRegistryType}_LIST)
+endmacro()
+
+#
+# Registry properties entries access
+#
+
+# Set a named Property entry in the named Registry of a RegistryType.
+macro(SetRegistryEntry InRegistryType InRegistryName InPropertyName InValue)
+	set(REGISTRY_${InRegistryType}_${InRegistryName}_${InPropertyName} "${InValue}" CACHE INTERNAL "")
+endmacro()
+
+# Add a named Property entry in the named Registry of a RegistryType.
+macro(AddRegistryEntry InRegistryType InRegistryName InPropertyName InValue)
+    set(VarName "REGISTRY_${InRegistryType}_${InRegistryName}_${InPropertyName}")
+    
+    if(DEFINED CACHE{${VarName}})
+        set(TempList "$CACHE{${VarName}}")
+    else()
+        set(TempList "")
+    endif()
+
+    list(APPEND TempList "${InValue}")
+    set(${VarName} "${TempList}" CACHE INTERNAL "")
+endmacro()
+
+# Get a named Property entry of the named Registry of a RegistryType.
+macro(GetRegistryEntry OutVar InRegistryType InRegistryName InPropertyName)
+	set(VarName "REGISTRY_${InRegistryType}_${InRegistryName}_${InPropertyName}")
+
+	if(DEFINED ${VarName})
+		set(${OutVar} "$CACHE${${VarName}}")
+	else()
+		set(${OutVar} "")
+	endif()
+endmacro()
